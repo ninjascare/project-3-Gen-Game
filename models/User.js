d0900ce607cb2c9685c33f679ad904b0139cba4a
Kevin Rosales <kevinrosales@Kevins-MacBook-Air.local>
2018-11-14 13:58:08 -0500

    created my user model

--- models/User.js
+++ models/User.js
@@ -1,6 +1,16 @@
 const mongoose = require("../db/connections");
 const Schema = mongoose.Schema;
 
-const User = new Schema({});
+const User = new Schema({
+  name: String,
+  console: String,
+  image: String,
+  games: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "VideoGame"
+    }
+  ]
+});
 
 module.exports = mongoose.model("User", User);

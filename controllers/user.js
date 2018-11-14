b8be5c5fc52a7afbaba208f6a8e1e36f2d1fb588
Kevin Rosales <kevinrosales@Kevins-MacBook-Air.local>
2018-11-14 14:40:47 -0500

    got my show and craete route and controller done

--- controllers/user.js
+++ controllers/user.js
@@ -3,7 +3,23 @@ const VideoGame = require("../models/VideoGame");
 
 const usersController = {
   index: (req, res) => {
-                      
+    User.find({}).then(users => {
+      res.send(users);
+    });
+  },
+  show: (req, res) => {
+    const userId = req.params.userId;
+    User.findById(userId)
+      .populate("games")
+      .then(user => {
+        res.send(user);
+      });
+  },
+  create: (req, res) => {
+    User.create(req.body)
+    .then(newUser => {
+      res.send(newUser);
+    });
   }
 };
 
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const usersController = require('../controllers/user')
-const videoGamesController = require('../controllers/videoGame')
+const usersController = require("../controllers/user");
+const videoGamesController = require("../controllers/videoGame");
 
 router.get("/api/users", usersController.index);
+router.post("/api/users/", usersController.create);
+router.get("/api/users/:userId", usersController.show);
+// router.patch("/api/users/:userId", usersController.update);
+// router.delete("/api/users/:userId", usersController.delete);
 
 module.exports = router;

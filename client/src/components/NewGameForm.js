dc76ea6b8a0db0eea8188435ce2ec62308195ab5
Kevin Rosales <kevinrosales@Kevins-MacBook-Air.local>
2018-11-20 16:19:30 -0500

    gfshdjzsk

--- client/src/components/NewGameForm.js
+++ client/src/components/NewGameForm.js
@@ -36,7 +36,7 @@ export default class NewGameForm extends Component {
     const userId = this.props.match.params.userId;
     axios.post(`/api/users/${userId}/videogames`, payload).then(res => {
       console.log(res.data);
-      this.props.history.go(`/users`);
+      this.props.history.push(`/users/${userId}`);
     }); 
   };
 

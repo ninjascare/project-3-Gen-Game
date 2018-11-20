2530bbbc94cddf8908a4b745b6ad6b14118c483f
Kevin Rosales <kevinrosales@Kevins-MacBook-Air.local>
2018-11-20 15:45:34 -0500

    fixed nav bar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
 import { Navbar, NavItem, Icon } from "react-materialize";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const NavBarItem = styled.div`
+  display: flex;
+  align-items: flex-end;
+`;
 
 export default class NavBar extends Component {
   render() {
@@ -8,10 +14,20 @@ export default class NavBar extends Component {
       <div>
         <Navbar className=" #78909c blue-grey lighten-1" right>
           <NavItem brand="GameLiker" className="brand-logo center ">
-           Gen-Game
+            Gen-Game
           </NavItem>
-         <Link to='/'> <NavItem  waves='light'><Icon left>home</Icon>Home</NavItem></Link>
-         <Link to='/users'><NavItem waves='light'><Icon left>supervisor_account</Icon>Users</NavItem></Link>
+          <NavBarItem>
+            <Link to="/">
+              <NavItem waves="light">
+                <Icon left>home</Icon>Home
+              </NavItem>
+            </Link>
+            <Link to="/users">
+              <NavItem waves="light">
+                <Icon left>supervisor_account</Icon>Users
+              </NavItem>
+            </Link>
+          </NavBarItem>
         </Navbar>
       </div>
     );
